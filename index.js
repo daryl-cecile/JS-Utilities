37597588fb669e78c651d832de87766967497b23
Daryl Cecile <6310278+daryl-cecile@users.noreply.github.com>
2021-02-03 16:39:55 +0000

    Added `GenIdentifier()` method for generating IDs

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ export function cloneObject(sourceObj) {
 		}
 		
 		if ( typeof obj === "object" && obj !== null && !obj.hasOwnProperty(CLONE_KEY) ){
-			obj[CLONE_KEY] = UID();
+			obj[CLONE_KEY] = GenIdentifier('xxxx-xxxx-xxxx-xxxx-xxxx');
 			toClean.push(obj);
 			clonedStubs[obj[CLONE_KEY]] = ret;
 		}
@@ -40,4 +40,30 @@ export function cloneObject(sourceObj) {
 	let final = innerClone(sourceObj);
 	toClean.forEach((obj) => delete obj[CLONE_KEY]);
 	return final;
+}
+
+/**
+ * Creates a roughly unique ID using a provided pattern, where `x` is to be replaced by a letter or digit
+ *
+ * @param {object} sourceObj - The object to be cloned
+ * @return {object} A clone of the source object
+ *
+ * @example
+ *	GetIdentifier('xxxx-xxxx-xxxxxxxx-xxxx-xxxx');
+ *
+ */
+function GenIdentifier(pattern){
+	let final = pattern;
+  
+	function mk(length){
+		let chars = (Math.random() + 1).toString(36).substr(2, length);
+		if (chars.length < length){
+			chars += mk(length - chars.length);
+  		}
+    		return chars;
+  	}
+  
+ 	return final.replace(/[x]+/gm, function(r){
+	    	return mk(r.length)
+  	});
 }
